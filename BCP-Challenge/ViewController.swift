c7631e6f1c706f645ea0fbfd6039962ccf758c67
Santiago A. Delgado <santi.d@hey.com>
2022-04-09 10:52:59 -0500

    Initial Commit

---
+++ BCP-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BCP-Challenge
+//
+//  Created by Santi D on 9/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
